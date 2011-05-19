72746b4bea3452c7ee18c147d714590215b2bf83
Rob Terhaar <rob@atlanticdynamic.com>
2011-05-19 17:56:00 -0400

    add debian os to case statement for apache config

--- foreman/manifests/passenger.pp
+++ foreman/manifests/passenger.pp
@@ -8,6 +8,7 @@ class foreman::passenger {
     path => $lsbdistid ? {
       default => "/etc/httpd/conf.d/foreman.conf",
       "Ubuntu" => "/etc/apache2/conf.d/foreman.conf"
+      "Debian" => "/etc/apache2/conf.d/foreman.conf"
     },
     content => template("foreman/foreman-vhost.conf.erb"),
     mode => 644,
