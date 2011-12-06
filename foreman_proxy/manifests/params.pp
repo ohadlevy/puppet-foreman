aee5be66d8a204415d04663c69ad56eb87454b8c
Ohad Levy <ohadlevy@gmail.com>
2011-12-06 12:09:47 +0200

    fixed wrnog directory name, foreman tries to create it under tftproot/boot

--- foreman_proxy/manifests/params.pp
+++ foreman_proxy/manifests/params.pp
@@ -20,7 +20,7 @@ class foreman_proxy::params {
   $syslinux_root  = "/usr/share/syslinux"
   $syslinux_files = ["pxelinux.0","menu.c32","chain.c32"]
   $tftproot       = $tftp::params::root
-  $tftp_dir       = ["${tftproot}/pxelinux.cfg","${tftproot}/build"]
+  $tftp_dir       = ["${tftproot}/pxelinux.cfg","${tftproot}/boot"]
 
   $dhcp = false
   $dns  = false
