77e83fd8207ecaba1ba3391f87eb766d4fbe875b
Ohad Levy <ohadlevy@gmail.com>
2012-01-15 11:28:47 +0200

    fixes #1409 - tftp boot files as links are not accessible

--- foreman_proxy/manifests/tftp.pp
+++ foreman_proxy/manifests/tftp.pp
@@ -2,22 +2,22 @@ class foreman_proxy::tftp {
   include ::tftp
 
   file{ $foreman_proxy::params::tftp_dir:
-    owner   => $foreman_proxy::params::user,
-    mode    => 644,
-    require => Class["foreman_proxy::install"],
     ensure  => directory,
+    owner   => $foreman_proxy::params::user,
+    mode    => '0644',
+    require => Class["foreman_proxy::install"],
     recurse => true;
   }
 
-  link_file{$foreman_proxy::params::syslinux_files:
+  sync_file{$foreman_proxy::params::syslinux_files:
     source_path => $foreman_proxy::params::syslinux_root,
     target_path => $foreman_proxy::params::tftproot,
     require     => Class["tftp::install"];
   }
 }
-define link_file($source_path, $target_path) {
+define sync_file($source_path, $target_path) {
   file{"$target_path/$name":
-    ensure => link,
-    target => "$source_path/$name"
+    ensure => present,
+    source => "$source_path/$name"
   }
 }
