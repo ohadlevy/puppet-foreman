fa49f29521a3c6204c4bdab3ab5b629a17c3fb32
Jan Vansteenkiste <jan@vstone.eu>
2012-01-19 12:29:50 +0100

    Puppet-Lint-I-Fied passenger module: Fix quoting and various other fixes

--- passenger/manifests/install.pp
+++ passenger/manifests/install.pp
@@ -1,7 +1,13 @@
 class passenger::install {
-  case $operatingsystem {
-    redhat,centos,fedora,Scientific: { include passenger::install::redhat }
-    Debian,Ubuntu: { include passenger::install::debian }
-    default: { fail("${hostname}: This module does not support operatingsystem $operatingsystem") }
+  case $::operatingsystem {
+    redhat,centos,fedora,Scientific: {
+      include passenger::install::redhat
+    }
+    Debian,Ubuntu: {
+      include passenger::install::debian
+    }
+    default: {
+      fail("${::hostname}: This module does not support operatingsystem ${::operatingsystem}")
+    }
   }
 }
