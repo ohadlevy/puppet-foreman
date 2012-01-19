577da04155f10b2efcd4a68c73f5ebda19a9d509
Jan Vansteenkiste <jan@vstone.eu>
2012-01-19 12:41:25 +0100

    Puppet-Lint-I-Fied apache module: Fix quoting and various other fixes

--- apache/manifests/install.pp
+++ apache/manifests/install.pp
@@ -1,8 +1,14 @@
 class apache::install {
-  case $operatingsystem {
-    redhat,centos,fedora,Scientific: { $http_package = "httpd" }
-    Debian,Ubuntu: { $http_package = "apache2" }
-    default: { fail("${hostname}: This module does not support operatingsystem $operatingsystem") }
+  case $::operatingsystem {
+    redhat,centos,fedora,Scientific: {
+      $http_package = 'httpd'
+    }
+    Debian,Ubuntu: {
+      $http_package = 'apache2'
+    }
+    default: {
+      fail("${::hostname}: This module does not support operatingsystem ${::operatingsystem}")
+    }
   }
 
   package { $http_package:
